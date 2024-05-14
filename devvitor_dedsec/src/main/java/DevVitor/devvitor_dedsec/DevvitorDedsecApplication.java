28f9dee28026766bcd1e4b43915342ffecb12cdc
vitor <zorinos.masterzorin@outlook.com>
2024-05-14 11:37:16 -0300

    Criado projeto SpringBoot com Java

---
+++ devvitor_dedsec/src/main/java/DevVitor/devvitor_dedsec/DevvitorDedsecApplication.java
@@ -0,0 +1,13 @@
+package DevVitor.devvitor_dedsec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevvitorDedsecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevvitorDedsecApplication.class, args);
+	}
+
+}
---
+++ devvitor_dedsec/src/test/java/DevVitor/devvitor_dedsec/DevvitorDedsecApplicationTests.java
@@ -0,0 +1,13 @@
+package DevVitor.devvitor_dedsec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevvitorDedsecApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
